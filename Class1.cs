1db1ecf6036bd75dad90ecc75ff1b278d9846725
SeungHyun <67039023+gusanans218@users.noreply.github.com>
2021-03-11 16:12:04 +0900

    20210311

---
+++ Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        //오버로딩
+        
+        static int add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static float add(float a, float b, float c , float d = 0f)
+        {
+            return a + b+c;
+        }
+        
+    static void Main(string[] args)
+    {
+            float a = 20;
+            float b = 40;
+            float c = 1.3f;
+
+            float d = Class1.add(a, b, c);
+            Console.WriteLine($"결과값1   {d}");
+
+
+            int num1 = 20;
+            int num2 = 30;
+            int num3 = Class1.add(num1, num2);
+
+            Console.WriteLine($"결과값2   {num3}");
+
+        }
+    }
+}
