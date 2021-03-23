d1993c842b1dec1beb49c5ff47d35319ff2e7810
SeungHyun <67039023+gusanans218@users.noreply.github.com>
2021-03-23 11:22:45 +0900

    is bool 20210323

--- Program.cs
+++ Program.cs
@@ -1,42 +1,62 @@
 ﻿using System;
 
-namespace ConsoleApp4
+namespace ConsoleApp5
 {
-    //클래스 형변환
-
     class Player
     {
-        protected int def;
-    }
-    class Myclass
-    {
-        void test()
-        {
-            def = 10;
-        }
+
+
     }
     class Knight:Player
     {
-        void test()
-        {
-            def = 10;
-        }
-        private int hp;
-        private int attakc;
 
-        public void setHP(int hp)
-        {
-            this.hp = hp;
-            Console.WriteLine($"hp값 : {this.hp}");
-        }
-        
     }
+    class Mage : Player
+    {
+        internal int mp;
+    }
+
     class Program
     {
-         
+     /*   static void EnterGame(Knight knight)
+        {
+
+        }
+        static void EnterGame(Mage mage)
+        {
+
+        }*/
+        static void EnterGame(Player player)
+        {
+            //is = bool형
+            bool isMage = (player is Mage);
+            if(isMage)
+            {
+                Mage mage2 = (Mage)player;
+                mage2.mp = 10;
+                Console.WriteLine("생성");
+            }
+            //as
+            Mage mage3 = (player as Mage);
+
+            if(mage3 != null)
+            {
+                mage3.mp = 10;
+                Console.WriteLine("as생성");
+            }
+        }
+
         static void Main(string[] args)
         {
             Knight knight = new Knight();
+            Mage mage = new Mage();
+
+            /*Player magePlayer = new Player();
+            Mage mage2 = (Mage)magePlayer;
+            mage2.mp = 10;*/
+
+            EnterGame(knight);
+            EnterGame(mage);
 
         }
     }
