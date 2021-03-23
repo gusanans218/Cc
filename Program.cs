d02a9330b0ca787cb076ac8374e9454f62d780b1
SeungHyun <67039023+gusanans218@users.noreply.github.com>
2021-03-23 09:22:58 +0900

    필드, static 210323

--- Program.cs
+++ Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 
-namespace ConsoleApp2
+namespace ConsoleApp3
 {
-    class Program
+    class Knight
     {
-        static void Main(string[] args)
+        public int hp;
+        public int attack;
+
+        public Knight()
         {
-            Console.WriteLine("Hello World!");
+            hp = 100;
+            attack = 10;
+            Console.WriteLine("생성되었습니다");
+        }
+
+        public Knight(int hp) : this()
+        {
+            hp = 100;
+            Console.WriteLine("생성되었습니다");
+        }
+        public Knight(int hp, int attack):this(hp)
+        {
+            this.hp = hp;
+            this.attack = attack;
+            Console.WriteLine("hp 생성자 호출");
+        }
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Knight knight = new Knight(50,50);
+
+            Console.WriteLine($"hp : {knight.hp} attack : {knight.attack}");
         }
     }
 }
